a9da15ddc1d665afd89240c700aa439a12e66ff6
maisjamil1 <maisjamil17118@gmail.com>
2020-03-01 16:34:57 +0200

    1

---
+++ app.js
@@ -0,0 +1,131 @@
+var names = [
+  "bag.jpg",
+  "banana.jpg",
+  "bathroom..jpg",
+  "boots.jpg",
+  "breakfast.jpg",
+  "bubblegum.jpg",
+  "chais.jpg",
+  "cthulhu.jpg",
+  "dog-duck.jpg",
+  "dragon.jpg",
+  "pen.jpg",
+  "pet-sweep..jpg",
+  "scissors.jpg",
+  "shark.jpg",
+  "sweep.png",
+  "tauntaun.jpg",
+  "unicorn.jpg",
+  "usb.gif",
+  "water-can.jpg",
+  "wine-glass.jpg",
+];
+
+//(1) get the images
+var leftImage = document.getElementById('leftImage');
+var imageCenter = document.getElementById('imageCenter');
+var rightImage = document.getElementById('rightImage');
+
+var imageSection = document.querySelector('#imagesSection')
+//____________________________________________________________
+
+//(2) add src,alt,title to the images to test if ever thing is working
+
+// leftImage.src = `img/${names[0]}.jpg`;
+// leftImage.alt = names[0];
+// leftImage.title = names[0];
+
+// imageCenter.setAttribute('src',`img/${names[1]}.jpg`);
+// imageCenter.alt = names[0];
+// imageCenter.title = names[0];
+
+// rightImage.setAttribute('src',`img/${names[1]}.jpg`);
+// rightImage.setAttribute('alt',names[1]);
+// rightImage.setAttribute('title',names[1]);
+
+//____________________________________________________________
+
+//(3_1) create constructor function for the products
+function Product(name) {
+  this.name = name;
+  this.clicks = 0;
+  this.views = 0;
+  this.imagePath = `img/${this.name}`;
+  Product.all.push(this);
+}
+Product.all =[];
+//______________________________________________________________
+//(3_2) instantiate objects for all the products one shot
+for(var i =0; i<names.length; i++) {
+  new Product(names[i]);
+}
+
+//(4) render 2 random images
+var leftProduct,centerProduct,righProduct;
+function render () {
+  leftProduct = Product.all[randomNumber(0,Product.all.length-1)];
+  console.log(leftProduct);
+
+
+   rightProduct = Product.all[randomNumber(0,Product.all.length-1)];
+  console.log(righProduct);
+
+
+
+  leftImage.setAttribute('src',leftProduct.imagePath);
+  leftImage.setAttribute('alt',leftGoat.name);
+  leftImage.setAttribute('title',leftGoat.name);
+
+
+
+
+
+  rightImage.setAttribute('src',rightGoat.imagePath);
+  rightImage.setAttribute('alt',rightGoat.name);
+  rightImage.setAttribute('title',rightGoat.name);
+}
+render();
+
+//(5) add the event listener to render new images
+imageSection.addEventListener('click',handleClickOnGoat);
+var totalClicks =0;
+function handleClickOnGoat(event) {
+  if(totalClicks <25 ) {
+    if(event.target.id !== 'imagesSection') {
+      if(event.target.id === 'leftImage') {
+        leftGoat.clicks++;
+      } else if(event.target.id === 'rightImage') {
+        rightGoat.clicks++;
+      }
+      totalClicks++;
+      leftGoat.views++;
+      rightGoat.views++;
+      render();
+    }
+  }  else {
+    console.log('more than 5 clicks');
+    imageSection.removeEventListener('click',handleClickOnGoat);
+    render2();
+  }
+}
+
+function render2() {
+  var ulE1 = document.getElementById('summary');
+  for (var i =0; i<Goat.all.length ; i++) {
+    var liE1 = document.createElement('li');
+    liE1.textContent = `${Goat.all[i].name} has ${Goat.all[i].clicks} clicks and ${Goat.all[i].views} views`;
+    ulE1.appendChild(liE1);
+  }
+}
+
+
+// (5) Where should we add the event listener(for the left or right/ to imagesSection will be better since we will have only one clickListener )
+// 5a identify the  clicked image
+// 5b keep track of how many times the image have been clicked and viewed?
+// 5c re render the images
+
+
+//helper functions
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
